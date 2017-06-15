7952d8f463375707393136db1da6e69c9a6a43d1
kbajno <kbajno@gmail.com>
2017-06-15 13:34:43 -0400

    PhotoActivity now has menu bar.

--- flickrviewer/src/main/java/com/example/flickrviewer/PhotoActivity.java
+++ flickrviewer/src/main/java/com/example/flickrviewer/PhotoActivity.java
@@ -1,11 +1,14 @@
 package com.example.flickrviewer;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by kbajn on 6/15/2017.
@@ -55,6 +58,30 @@ public class PhotoActivity extends AppCompatActivity implements SharedPreference
     public boolean onSupportNavigateUp(){
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+
+            case R.id.action_favorite:
+                Intent favIntent = new Intent(this, FavActivity.class);
+                startActivity(favIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
